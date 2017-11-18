ca7cf3cb27af795f4f8503b8c37cfa6f507b00ae
rajshah1 <shahraj121997@gmail.com>
2017-11-18 08:35:49 +0530

    Update Favourites+CoreDataClass.swift

--- Near Me/Favourites+CoreDataClass.swift
+++ Near Me/Favourites+CoreDataClass.swift
@@ -2,14 +2,13 @@
 //  Favourites+CoreDataClass.swift
 //  
 //
-//  Created by Anshul Shah on 14/11/17.
+//  Created by raj Shah on 14/11/17.
 //
 //
 
 import Foundation
 import CoreData
 
-@objc(Favourites)
-public class Favourites: NSManagedObject {
+@objc(Favourites) {
 
 }
